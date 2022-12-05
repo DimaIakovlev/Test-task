88524c65953a261787dd427c2c894fdcbdef2473
Yakovlev Dima <zif18@rambler.ru>
2022-12-05 23:19:35 +0500

    test programm

--- Program.cs
+++ Program.cs
@@ -6,14 +6,26 @@ string[] newArray = new string[5];
 
 for (int i = 0; i < baseArray.Length; i++)
 {
-    Console.Write($"Array[{i}] = ");
+    Console.Write($"Array[{i + 1}] = ");
     baseArray[i] = Console.ReadLine();
 }
+
 for (int i = 0; i < baseArray.Length; i++)
 {
     if (baseArray[i].Length <= 3)
     {
         newArray[i] = baseArray[i];
     }
+}
 
+Console.WriteLine("Элементы массива, длинна которых меньше или равна трем: ");
+
+for (int i = 0; i < newArray.Length; i++)
+{
+    if (newArray[i] == null) ;
+
+    else
+    {
+        Console.WriteLine($"Array[{i + 1}] = {newArray[i]}");
+    }
 }
