99b14b24c2450f47ab51558406ef51c4c5c856d1
Yakovlev Dima <zif18@rambler.ru>
2022-12-05 21:19:31 +0500

    input arrayA

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите члены массива через запятую");
+string temp = Console.ReadLine();
+string[] arrayA = temp.Split(new Char[] { ' ' });
+string[arrayA.GetLength()] arrayB = "";
+for (int i = 0; i < arrayA.GetLength(); i++)
+{
+    if (arrayA.GetLength(i) == 3)
+    {
+        arrayB[i] = arrayA[i];
+    }
+}
+
+for (int i = 0; i < arrayB.GetLength(); i++)
+{
+    Console.Write(arrayB[i]);
+}
